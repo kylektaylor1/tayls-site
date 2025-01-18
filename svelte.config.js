6d5650809aeee41c9ccc9531295a958fa29dabea
Kyle Taylor <kylektaylor1@gmail.com>
2025-01-17 22:30:30 -0500

    feat: yeet sst

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from 'svelte-kit-sst';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,9 +9,6 @@ const config = {
     preprocess: [vitePreprocess(), mdsvex()],
 
     kit: {
-        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
         adapter: adapter()
     },
 
